c3dbb440bbce79e045c25e7127a4d2f73d273faa
Abraxas-7 <arber.beshaj.02@gmail.com>
2024-11-12 16:11:39 +0100

    es3

--- Snack3/script/script.js
+++ Snack3/script/script.js
@@ -12,12 +12,14 @@ function getRandomNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+// Creo un array contenente 100 numeri random tra 1 e 100
 let myArray = [];
 
 for (let i = 0; i < 100; i++) {
   myArray.push(getRandomNumber(1, 100));
 }
 
+// Genero due numeri random per l'intervallo
 let a,b;
 
 do {
@@ -25,9 +27,8 @@ do {
     b = getRandomNumber(0, 100);
 }   while (a > b);
 
-
-console.log(a, b);
+// Stampo l'array iniziale, l'intervallo e il mio array
+console.log(myArray);
+console.log(`Intervallo: [${a}, ${b}]`);
 console.log(sottoarray(myArray, a, b));
-
-
 
